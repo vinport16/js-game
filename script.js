cd27f52323187cef2bcc886c11be487f17800741
Vincent Portelli <vinport16@gmail.com>
2018-06-10 17:38:37 -0400

    added fusion generator to pause

--- script.js
+++ script.js
@@ -286,6 +286,7 @@ function pause(){
     document.getElementById("chaingunTower").disabled = true;
     document.getElementById("seekingTower").disabled = true;
     document.getElementById("repairBuilding").disabled = true;
+    document.getElementById("fusionGenerator").disabled = true;
     document.getElementById("relay").disabled = true;
     document.getElementById("pause").innerHTML = "pause";
   }else{
@@ -298,6 +299,7 @@ function pause(){
     document.getElementById("chaingunTower").disabled = false;
     document.getElementById("seekingTower").disabled = false;
     document.getElementById("repairBuilding").disabled = false;
+    document.getElementById("fusionGenerator").disabled = false;
     document.getElementById("relay").disabled = false;
     document.getElementById("pause").innerHTML = "resume";
   }
